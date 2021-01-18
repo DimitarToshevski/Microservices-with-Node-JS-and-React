76412f0f0ede4dd16ac5ae58a7f489731c24e20c
war_master@bitex.bg <stu2001717002@uni-plovdiv.bg>
2021-01-18 22:23:57 +0200

    Typed the user model

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,5 +1,22 @@
 import mongoose from 'mongoose';
 
+// An interface that describes the properties that are required to create a new User
+interface IUser {
+    email: string;
+    password: string;
+}
+
+// An interface that describes the properties that a User model has
+interface IUserModel extends mongoose.Model<IUserDoc> {
+  build(attrs: IUser): IUserDoc;
+}
+
+// An interface that describes the properties that a User document has
+interface IUserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +28,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: IUser) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<IUserDoc, IUserModel>('User', userSchema);
 
 export { User };
