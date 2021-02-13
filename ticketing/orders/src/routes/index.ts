3b8ecffec8f2eab29926f48f1dbbca9c9a2da2ab
war_master@bitex.bg <war_master@bitex.bg>
2021-02-13 12:20:32 +0200

    Update the orders index

--- ticketing/orders/src/routes/index.ts
+++ ticketing/orders/src/routes/index.ts
@@ -5,7 +5,7 @@ import { Order } from '../models/order';
 const router = express.Router();
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
-  const orders = await Order.find({});
+  const orders = await Order.find({userId: req.currentUser!.id}).populate('ticket');
 
   res.status(200).send(orders);
 });
