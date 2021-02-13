a0ab2567df9bf6a6bd9c2601d8772cb6767aee9f
war_master@bitex.bg <war_master@bitex.bg>
2021-02-13 12:31:43 +0200

    Add more unit tests for index

--- ticketing/orders/src/routes/__test__/index.test.ts
+++ ticketing/orders/src/routes/__test__/index.test.ts
@@ -32,13 +32,13 @@ it('fetches orders for a particular user', async () => {
   // Create two orders as User #2
   const userTwoCookie = global.signup();
 
-  await request(app)
+  const { body: userTwoOrderOne } = await request(app)
     .post('/api/orders')
     .set('Cookie', userTwoCookie)
     .send({ ticketId: ticketTwo.id })
     .expect(201);
 
-  await request(app)
+  const { body: userTwoOrderTwo } = await request(app)
     .post('/api/orders')
     .set('Cookie', userTwoCookie)
     .send({ ticketId: ticketThree.id })
@@ -53,4 +53,6 @@ it('fetches orders for a particular user', async () => {
     .expect(200);
 
     expect(userTwoOrdersResponse.body.length).toEqual(2);
+    expect(userTwoOrdersResponse.body[0]).toEqual(userTwoOrderOne);
+    expect(userTwoOrdersResponse.body[1]).toEqual(userTwoOrderTwo);
 });
