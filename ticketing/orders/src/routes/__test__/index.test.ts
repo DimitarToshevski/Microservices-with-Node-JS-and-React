e12036af386d53877317572a20661fa85accee08
war_master@bitex.bg <war_master@bitex.bg>
2021-02-13 12:28:27 +0200

    Add tests for the index

---
+++ ticketing/orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,56 @@
+import { OrderStatus } from '@dt-ticketing/common';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+
+  await ticket.save();
+
+  return ticket;
+}
+
+it('fetches orders for a particular user', async () => {
+  // Create three tickets
+  const ticketOne = await buildTicket()
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  // Create one order as User #1
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // Create two orders as User #2
+  const userTwoCookie = global.signup();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwoCookie)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwoCookie)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  // Make request to get orders for User #2
+
+  const userTwoOrdersResponse = await request(app)
+    .get('/api/orders')
+    .set('Cookie', userTwoCookie)
+    .send()
+    .expect(200);
+
+    expect(userTwoOrdersResponse.body.length).toEqual(2);
+});
